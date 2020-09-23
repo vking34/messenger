7fcd86b660d4a803004cfe3259f05f4680fe248d
dev_vuong <lzzvkingzzl@gmail.com>
2020-09-23 15:19:59 +0700

    chore: rmd

--- src/app.ts
+++ src/app.ts
@@ -21,30 +21,7 @@ io.on('connection', (socket) => {
 
     // when the client emits 'new message', this listens and executes
     socket.on('new message', async (data) => {
-        // ver 1
-        // get receiver socket id in cache
-        // let receiverSocketId: string = await redisClient.getAsync(data.receiver);
-        // console.log(data);
-        // console.log('target socket:' + receiverSocketId);
-
-        // if (receiverSocketId) {
-        //     io.to(receiverSocketId).emit('new message', data);
-        // }
-
-        // ver 2
-        // console.log(data);
-        // const { receiver } = data;
-        // socket.join(receiver);
-
-        // io.to(receiver).emit('new message', data);
-        
-
-        // ver 3
         const { sender, receiver } = data;
-        // if (sender.localeCompare(receiver) > 0)
-        //     var roomId: string = sender + receiver;
-        // else
-        //     var roomId: string = receiver + sender;
         
         console.log(data);
         let receiverSockets = await redisClient.lrangeAsync(receiver, 0, -1);
@@ -67,16 +44,6 @@ io.on('connection', (socket) => {
         if (addedUser) return;
 
         const { sender } = data;
-        
-        // const { sender, receiver } = data;
-        // if (sender.localeCompare(receiver) > 0)
-        //     var roomId: string = sender + receiver;
-        // else
-        //     var roomId: string = receiver + sender;
-
-        // console.log(roomId);
-        // socket.join(roomId);
-
         // store the username in the socket session for this client
         socket.username = sender;
         ++numUsers;
@@ -99,13 +66,6 @@ io.on('connection', (socket) => {
 
     // when the client emits 'typing', we broadcast it to others
     socket.on('typing', async (data) => {
-        // let receiverSocketId: string = await redisClient.getAsync(data.receiver);
-        // if (receiverSocketId)
-        //     io.to(receiverSocketId).emit('typing', data);
-
-        // const { receiver } = data;
-        // io.to(receiver).emit('typing', data);
-
         const { sender, receiver } = data;
         console.log(data, " typing");
         
@@ -123,12 +83,6 @@ io.on('connection', (socket) => {
 
     // when the client emits 'stop typing', we broadcast it to others
     socket.on('stop typing', async (data) => {
-        // let receiverSocketId: string = await redisClient.getAsync(data.receiver);
-        // if (receiverSocketId)
-        //     io.to(receiverSocketId).emit('stop typing', data);
-        // const { receiver } = data;
-        // io.to(receiver).emit('stop typing', data);
-
         const { sender, receiver } = data;
         console.log(data, " stop typing");
 
@@ -151,9 +105,6 @@ io.on('connection', (socket) => {
             console.log(socket.username, ' disconnected!');
 
             redisClient.lremAsync(socket.username, 0, socket.id);
-            // socket.leave(socket.username);
-
-            // echo globally that this client has left
             socket.broadcast.emit('user left', {
                 username: socket.username,
                 numUsers: numUsers
