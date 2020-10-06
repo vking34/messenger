9b8be71bc32d6a7de1ff5d6ce93d764340b96a97
dev_vuong <lzzvkingzzl@gmail.com>
2020-10-06 17:11:19 +0700

    fix(socket): fix cors

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
-app.options('*', cors());   // cors for sockets
 
 // sockets
 require('./sockets/index');
