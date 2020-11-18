a72ff9fe5bebe37381c92bdfdc9233655bff0bb0
dev_vuong <lzzvkingzzl@gmail.com>
2020-11-18 14:46:40 +0700

    fix(auction-socket): disable event

--- src/sockets/newAuctionResultEvent.ts
+++ src/sockets/newAuctionResultEvent.ts
@@ -1,10 +1,10 @@
-import { auctionNamespace } from './index';
-import { AuctionResult } from '../interfaces/auctionResult';
+// import { auctionNamespace } from './index';
+// import { AuctionResult } from '../interfaces/auctionResult';
 
-const NEW_AUCTION_RESULT = 'new_auction_result';
-export default (auctionResut: AuctionResult) => {
-    // console.log('auction id:', auctionResut.id);
-    auctionNamespace
-        .in(auctionResut.id.toString())
-        .emit(NEW_AUCTION_RESULT, auctionResut);
-}
+// const NEW_AUCTION_RESULT = 'new_auction_result';
+// export default (auctionResut: AuctionResult) => {
+//     // console.log('auction id:', auctionResut.id);
+//     auctionNamespace
+//         .in(auctionResut.id.toString())
+//         .emit(NEW_AUCTION_RESULT, auctionResut);
+// }
